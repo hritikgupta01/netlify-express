ed35b67f9e0a166d61732a4926202a1f956865c5
hritikgupta01 <87356814+hritikgupta01@users.noreply.github.com>
2023-01-04 12:43:43 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,54 +1,4 @@
-
-const express = require("express");
-const serverless = require("serverless-http");
-
-const app = express();
-const router = express.Router();
-const http = require('http').createServer(app)
-const PORT = process.env.PORT || 3000
-
-const io = require('socket.io')(http)
-
-
-const users = {};
-
-
-
- io.on('connection', socket=>{
-    console.log("connected");
-     socket.on('new-user-joined', name=>{
-         console.log("New user", name);
-         users[socket.id] = name;
-         socket.broadcast.emit('user-joined', name);
-     });
-
-     socket.on('send', message=>{
-         socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
-     });
-
-     socket.on('disconnect', message=>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
- })
-
-router.get("/", (req, res) => {
-  res.json({
-    hello: "Listening on ${PORT} hritik123"
-  });
-});
-app.use(`/.netlify/functions/api`, router);
-
-module.exports = app;
-module.exports.handler = serverless(app);
-
-
-
-
-
-
-
-/* node server which will handle socket io connections
+// node server which will handle socket io connections
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
@@ -72,7 +22,7 @@ const users = {};
 app.get('/', (req, res) => {
  
     // Sending the response
-    res.send('Hello World! Listening on port ${PORT}')
+    res.send('Hello World!')
     
     // Ending the response
     res.end()
@@ -95,6 +45,3 @@ app.get('/', (req, res) => {
         delete users[socket.id];
     });
  })
-app.use(`/.netlify/functions/api`, router);
-
-*/
